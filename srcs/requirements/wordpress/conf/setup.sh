b4724b29b0d911b6070c138362412af8e991c2d4
bou <aitbougnsa.ismail@gmail.com>
2025-11-13 19:11:16 +0100

    Fix some error in wordpress

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,30 +1,28 @@
-
+#!/bin/bash
 set -e
-trap "exit" TERM
 
-until mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
-  sleep 2
-done
+# Wait for MariaDB to be ready
+sleep 10
 
-cd /var/www/html/wordpress
-if [ ! -f "wp-config.php" ]; then
-    rm -rf *
-    wp core download --allow-root
-    wp config create --allow-root --dbname="$DB_NAME" \
-        --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST"
+# Download WordPress core
+wp core download --allow-root
 
-    wp core install --allow-root \
-        --url="$WP_URL" \
-        --title="$WP_TITLE" \
-        --admin_user="$WP_ADMIN_USER" \
-        --admin_password="$WP_ADMIN_PASSWORD" \
-        --admin_email="$WP_ADMIN_EMAIL"
+# Create wp-config.php
+wp config create --allow-root \
+  --dbname=$SQL_DATABASE \
+  --dbuser=$SQL_USER \
+  --dbpass=$SQL_PASSWORD \
+  --dbhost=mariadb:3306
 
-    wp user create --allow-root "$WP_USER" "$WP_EMAIL" \
-            --user_pass="$WP_PASSWORD" --role=editor
+# Install WordPress
+wp core install --allow-root \
+  --url=$WP_URL \
+  --title=$WP_TITLE \
+  --admin_user=$WP_ADMIN_USER \
+  --admin_password=$WP_ADMIN_PASSWORD \
+  --admin_email=$WP_ADMIN_EMAIL
 
-    wp theme activate twentytwentythree --allow-root 
-fi
+# Create a secondary user
+wp user create $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD --allow-root
 
-mkdir -p /run/php
-exec /usr/sbin/php-fpm7.4 -F
+php-fpm7.4 -F
